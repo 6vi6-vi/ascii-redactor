d802b7e711359345446c2bf81be0300f5d86c877
VI <fly.to.be.happy.2021@gmail.com>
2026-05-18 03:41:08 +0300

    Update .gitignore

--- ASCII-redactor/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/PCS/4 semester/new-project/ASCII-redactor/ASCII-redactor
-# Build directory: C:/PCS/4 semester/new-project/ASCII-redactor/ASCII-redactor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- ASCII-redactor/build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/PCS/4 semester/new-project/ASCII-redactor/ASCII-redactor/tests
-# Build directory: C:/PCS/4 semester/new-project/ASCII-redactor/ASCII-redactor/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ascii_paint_tests "C:/PCS/4 semester/new-project/ASCII-redactor/ASCII-redactor/build/tests/ascii_paint_tests.exe")
-set_tests_properties(ascii_paint_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/PCS/4 semester/new-project/ASCII-redactor/ASCII-redactor/tests/CMakeLists.txt;61;add_test;C:/PCS/4 semester/new-project/ASCII-redactor/ASCII-redactor/tests/CMakeLists.txt;0;")
-subdirs("../_deps/catch2-build")
